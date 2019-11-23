2c43bf0eb90e86916c17fb3dbb78e65b21d1b9e2
García Rodríguez Alan Humberto <54951428+AlanSennin@users.noreply.github.com>
2019-11-22 16:54:12 -0800

    T02.-Exception-p2
    Parte dos de lo visto en clase usando try.Parse

---
+++ Errores/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Errores
+{
+    public class Estudiante
+    {
+        //Encapsulando atributos de un Estudiante
+        public int Matricula { get; set; }
+        public string Nombre { get; set; }
+        public string Semestre { get; set; }
+        public string Carrera { get; set; }
+    }
+}
---
+++ Errores/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Errores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Estudiante = new List<Estudiante>(); //Creamos la variable "Estudiante" y le asignamos la instancia de la nueva lista
+
+            var Agregar = true; //Creamos la variable "Agregar" y la asignamos como true
+
+            while (Agregar) //Ciclo while en la variable "Agregar"
+            { 
+                try //Try-Cath por si ingresamos por error letras en la matricula...
+                {
+                    int Matricula = 0; //Creamos una variable Matricula como int y la igualamos a 0
+
+                    var E = new Estudiante(); //Instanciamos la clase "Estudiante" como E
+
+                    Console.WriteLine(""); //cw para pasar de linea...
+
+                    Console.WriteLine("Ingrese Matricula:");
+                    bool result = int.TryParse(Console.ReadLine(), out Matricula); //Creamos una variable "resul" como bool y aplicamos el int.TryParse, pidiendo la Matricula al usuario
+                    Console.WriteLine(""); //cw para pasar de linea...             //seguido del metodo out usando la variable "Matricula" que esta igualada a 0
+
+                    if (result == false) //Condicion por si la variable "resul" es falsa
+                    {
+                        Console.WriteLine("Ingrese un numero..."); //Mensaja de aviso...
+                    }
+
+                    Console.WriteLine(""); //cw para pasar de linea...
+
+                    Console.WriteLine("Inrgese Nombre:");
+                    E.Nombre = Console.ReadLine(); //Pedimos al usuario el Nombre
+                    Console.WriteLine(""); //cw para pasar de linea...
+
+                    Console.WriteLine("Ingrese Semestre:");
+                    E.Semestre = Console.ReadLine(); //Pedimos al usuario el Semestre
+                    Console.WriteLine(""); //cw para pasar de linea...
+
+                    Console.WriteLine("Ingrese Carrera:");
+                    E.Carrera = Console.ReadLine(); //Pedimos al usuario la Carrera
+                    Console.WriteLine(""); //cw para pasar de linea...
+
+                    Estudiante.Add(E); //Agregamos los valores ingresado de la instancia "E" a la lista "Estudiante"
+
+                    Console.WriteLine("¿Quiere agregar otro usuario?    Y/N"); //Preguntamos al usuario...
+
+                    if (Console.ReadLine() !="Y") //Condicion por si respondemos que si
+                    {
+                        Agregar = false; //Cambiamos de true a false la variable Agregar
+                    }
+
+                }
+                catch (Exception) //Si cometemos el error entonces se desplega el mensaje
+                {
+                    Console.WriteLine("Error agregando el usuario, intente nuevamente..."); //Mensaje de aviso...
+                    Console.ReadKey(); //Console ReadKey para que no se cierre el programa
+
+                }
+            }
+        }
+    }
+}
